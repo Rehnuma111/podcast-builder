fb0cb14f7e6970a4f57c4fdfd2cfd57c12eaf14c
Rehnuma69 <70592871+Rehnuma69@users.noreply.github.com>
2022-04-02 11:20:46 +0530

    audio

--- fronted/src/components/listenPodcast.js
+++ fronted/src/components/listenPodcast.js
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import app_config from "../config";
 
-
-// import ReactAudioPlayer from 'react-audio-player';
 const ListenPodcast = () => {
+  const url = app_config.api_url;
   const [podcast, setPodcast] = useState({});
   const [loading, setLoading] = useState(true);
 
   const { id } = useParams();
 
+  const fetchPodcastId = () => {
+    fetch(url + "/getbyid/" + id)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPodcast(data);
+        setLoading(false);
+      });
+  };
+
+  useEffect(()=>{
+    fetchPodcastId();
+  },[]);
+
   return (
     <>
+       <audio src={url + "/" + podcast.file}></audio>
       
-      {/* <ReactAudioPlayer
-                src="http://localhost:5000/music.mp3"
-                autoPlay
-                controls
-            /> */}
     </>
   );
 };
